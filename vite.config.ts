9cef1c098746d66140c31872ff31e6e660f097c8
IMO_player <sunnygreen0828@gmail.com>
2026-08-01 06:58:53 +0800

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { sites } from "./build/sites-vite-plugin";
 export default defineConfig({
+  base: "/GBA_AI_intro/",
   plugins: [react(), sites()],
   server: {
     host: "127.0.0.1",
